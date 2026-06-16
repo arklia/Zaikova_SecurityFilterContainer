a13c6b79a31978d071ea0f2cd2c5308c4f1be506
Zaikova V <154669290+arklia@users.noreply.github.com>
2026-06-16 12:07:40 +0000

    Дополненные комментарии

--- security_filter/apps.py
+++ security_filter/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+#Задаем конфигурацию приложения
 class SecurityFilterConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'security_filter'
